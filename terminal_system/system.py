4e1ed798e91b7aeea310401cdd4b18c0b88af6ac
Victor Ribeiro <ribeir145@gmail.com>
2022-01-18 18:00:13 -0300

    Menu resolvido.

--- terminal_system/system.py
+++ terminal_system/system.py
@@ -10,10 +10,19 @@ def menu():
         "(5)Visualizar Consultor\n",
         "(s)Sair do Sistema\n")
         inp = input()
-        if inp == 1:post_consultor()
-        if inp == 2:put_cliente()
-        if inp == 3:get_contrato()
-        if inp == 4:get_consultores()
-        if inp == 5: get_consultor()
-        if inp == s:break
-        else:print('Comando Inválido')
+        if inp == '1':
+            post_consultor()
+        if inp == '2':
+            put_cliente()
+        if inp == '3':
+            get_contrato()
+        if inp == '4':
+            get_consultores()
+        if inp == '5':
+            get_consultor()
+        if inp == 's':
+            break
+        else:
+            print('Comando Inválido')
+
+menu()
